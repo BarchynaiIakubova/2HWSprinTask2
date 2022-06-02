86ed9a1fa06df934d02556ab4713c10e51924eb0
barchynaiiakubova <barchynai.iakubova@gmail.com>
2022-06-02 13:48:25 +0600

    2HWSpring22

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -3,15 +3,36 @@ package peaksoft.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import peaksoft.model.Island2;
-import peaksoft.model.Wood3;
+import peaksoft.model.*;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
 
-    @Bean
-    public static Island2 getIsland(Wood3 wood) {
-        return new Island2(wood);
-    }
+//    @Bean
+//    public static Ocean1 ocean1() {
+//        return new Ocean1();
+//    }
+
+//    @Bean
+//    public static Island2 getIsland(Wood3 wood) {
+//        return new Island2(wood);
+//    }
+//    @Bean
+//    public static Wood3 wood3(Rabbit4 rabbit) {
+//        return new Wood3(rabbit);
+//    }
+//
+//    @Bean
+//    public static Rabbit4 rabbit4() {
+//        return new Rabbit4();
+//    }
+//
+//    @Bean
+//    public static Duck5 duck5() {
+//        return new Duck5();
+//    }
+
+
+
 }
